79f38c79a8dac88d9c2974aa10a822aabf355e8a
Raunak Gupta <106958764+raunak87233@users.noreply.github.com>
2024-04-26 09:51:45 +0000

    Conversion of datatypes

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,12 @@
+let score = 33
+//console.log(typeof score)
+//console.log(typeof (score))
+let ValueInNumber = Number(score)
+//console.log(score)
+let IsLoggedIn = 1
+booleanIsLoggedIn = Boolean(IsLoggedIn)
+//console.log(booleanIsLoggedIn)
+let someNumber = 33
+stringNumber = Number(string)
+console.log(typeof stringNumber)
+
