fd56fff3938fd3bc3d86b433e44684595672bb11
Raunak Gupta <106958764+raunak87233@users.noreply.github.com>
2024-04-26 13:02:17 +0000

    operators

---
+++ 01_basics/03_operations.js
@@ -0,0 +1,10 @@
+let value = 3
+let negvalue = -value
+//console.log(negvalue)
+//console.log("2"+1+3)
+//console.log(2+1+"3")
+console.log(+true)
+console.log(+"")
+let gamecounter =100
+gamecounter++
+console.log(gamecounter)
