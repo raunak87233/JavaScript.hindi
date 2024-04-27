94b1364fc3904f0226fadf7ca8af910560a4a9de
Raunak Gupta <106958764+raunak87233@users.noreply.github.com>
2024-04-27 04:13:02 +0000

    Datatype-Summary

---
+++ 01_basics/05_Datatypes-summary.js
@@ -0,0 +1,12 @@
+let myname = "Raunak Gupta"
+let anothername = myname
+//console.log(myname)
+let userOne = {
+    email : "user@gmail.com",
+    upi : "user@ybl"
+
+}
+let userTwo = userOne
+userTwo.email = "Raunak@gmail.com"
+console.log(userOne.email)
+console.log(userTwo.email)
