c4bb8ccdb716118b0af33644d15ad301e6de222b
Raunak Gupta <106958764+raunak87233@users.noreply.github.com>
2024-04-27 05:19:35 +0000

    about Strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,12 @@
+const name = "Raunak"
+console.log(name.split('u'))
+const repoCount = 50
+//console.log(name+repoCount+"value")
+//console.log('Hello my name is ${name} and my repo count is ${repoCount}')
+const gamename = new String('Raunak')
+//console.log(gamename[0]);
+//console.log(gamename.toUpperCase());
+//console.log(gamename.charAt(3));
+//console.log(gamename.indexOf('u'));
+const newString = gamename.substring(0, 4)
+//console.log(newString)
