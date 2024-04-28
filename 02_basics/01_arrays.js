5b2f1893e8f8a839339a06f11cb84f95b146c9ef
Raunak Gupta <106958764+raunak87233@users.noreply.github.com>
2024-04-28 05:45:31 +0000

    Arrays part1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,19 @@
+//let myArray = ['0', '1', '2', '3', '4', '5', '6']
+//console.log(myArray[2]);
+const myArray2 = new Array(1, 2, 3, 4)
+//console.log(myArray2[3]);
+//myArray2.pop()
+//myArray2.unshift(1000000)
+//myArray2.shift()
+
+//console.log(myArray2.includes(9));
+//console.log(myArray2.indexOf(3));
+// slice    //splice
+console.log("A", myArray2)
+const myn1 = myArray2.slice(1, 3)
+console.log(myn1)
+console.log("B", myArray2)
+const myn2 = myArray2.splice(1, 3)
+console.log("C", myArray2)
+console.log(myn2)
+
