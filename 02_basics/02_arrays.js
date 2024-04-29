4528098c543248e6184faff36323bebc990fe536
Raunak Gupta <106958764+raunak87233@users.noreply.github.com>
2024-04-29 05:53:19 +0000

    objects

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,23 @@
+const marvel_heros = ['Thor', 'Ironman', 'Superman']
+const dc_heros = ['Spiderman', 'Flash', 'Batman']
+//marvel_heros.push(dc_heros)
+//console.log(marvel_heros)
+const group_of_heros = marvel_heros.concat(dc_heros)
+//console.log(group_of_heros)
+//const all_new_heros = [...marvel_heros,...dc_heros]
+//console.log(all_new_heros);
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+//console.log(real_another_array);
+//console.log(Array.isArray("Raunak"))
+//console.log(Array.from("RAunak"))
+let score1 = 10002833
+let score2 = 6547382293
+let score3 = 53737635373
+let score4 = 453636737373
+let score5 = 536373838353
+let score6 = 23235373844
+let score7= 5363637373784
+console.log(Array.of(score1,score2,score3,score4,score5,score6,score7,))
+
