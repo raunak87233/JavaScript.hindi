b1d2eea5d3a3c4c73e3e11872817ff07b100c910
Raunak Gupta <106958764+raunak87233@users.noreply.github.com>
2024-05-01 05:18:49 +0000

    learning of Functions, Scopes, Arrow, IIFE

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,90 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        //console.log(username);
+    }
+    // console.log(website);
+
+     //two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+//console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+//addTwo(5)
+//const addTwo = function(num){
+    //return num + 2
+//}
+function one() {
+    const username = "Raunak"
+    function two(){
+        const website ="SA"
+        console.log(username)
+    }
+      //console.log(website)
+    // two()
+
+
+}
+//one()
+
+if(true){
+    const username = "Raunak"
+    if(username === "Raunak"){
+        const website = "Dus"
+        //console.log(username+website)
+    }
+    //console.log(website)
+}
+//console.log(username)
+//++++++++++++++++++++++++interesting ++++++++++++++++++++++++++++++++++++++
+
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+const addTwo = function(num){
+    return num + 2
+
+}
---
+++ 03_basics/03_Arrow.js
@@ -0,0 +1,41 @@
+const user = {
+    username : "Raunak",
+    price : 999,
+    welcomemessage : function(){
+        //console.log(`${this.username}, welcome to the website`)
+    }
+}
+//user.welcomemessage()
+//console.log(this)
+function chai(){
+    usernmae = "Raunak"
+    //console.log(usernmae)
+
+}
+//chai()
+//const code = function(){
+    //usernmae = "Raunak"
+    //console.log(this.username)
+//}
+const code = () => {
+    usernmae = "Raunak"
+    //console.log(this.username)
+}
+const addTwo = (num1, num2) => {
+    return num1 + num2
+}
+//console.log(addTwo(3, 4))
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+//const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+//console.log(addTwo(3, 4))
+
+
+const myArray = [2, 5, 3, 7, 8]
+ myArray.forEach()
